8c75acd37523077dd15d058413995ee900f1851b
Karan Parashar <whyredfire@gmail.com>
2021-07-02 14:56:37 +0530

    ginkgo: rebrand overlays for SparkOS
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ PRODUCT_BOARD_PLATFORM := trinket
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-spark
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
