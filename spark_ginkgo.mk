75cf9213102825307a474f0822679e63b90741a9
Karan Parashar <whyredfire@gmail.com>
2021-07-08 15:27:04 +0530

    ginkgo: Update fingerprint to Pixel 5 (RQ3A.210705.001)

--- spark_ginkgo.mk
+++ spark_ginkgo.mk
@@ -38,8 +38,8 @@ SPARK_BUILD_TYPE := OFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005 7349499 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210705.001 7380771 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
